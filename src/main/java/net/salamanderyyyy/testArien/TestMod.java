7f1c6c6c201edb11738f6a99ad5b96e7398cd436
salamanderyyyy <chiendailin@gmail.com>
2025-03-29 00:09:46 -0700

    forgot to add message for previous commit: added arien essence item, arien sword item, and arien block
    current: added creative mode tab

--- src/main/java/net/salamanderyyyy/testArien/TestMod.java
+++ src/main/java/net/salamanderyyyy/testArien/TestMod.java
@@ -1,6 +1,7 @@
 package net.salamanderyyyy.testArien;
 
 import net.salamanderyyyy.testArien.block.ModBlocks;
+import net.salamanderyyyy.testArien.item.ModCreativeModeTabs;
 import net.salamanderyyyy.testArien.item.ModItems;
 import org.slf4j.Logger;
 
@@ -58,6 +59,8 @@ public class TestMod {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
---
+++ src/main/java/net/salamanderyyyy/testArien/item/ModCreativeModeTabs.java
@@ -0,0 +1,31 @@
+package net.salamanderyyyy.testArien.item;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.salamanderyyyy.testArien.TestMod;
+import net.salamanderyyyy.testArien.block.ModBlocks;
+
+import java.util.function.Supplier;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TestMod.MODID);
+
+    public static final Supplier<CreativeModeTab> TESTNMOD_ITEMS_TAB = CREATIVE_MODE_TAB.register("testmod_items_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.ESSENCE.get()))
+                    .title(Component.translatable("creativetab.saltestingmod.testmod_items"))
+                    .displayItems((parameters, output) -> {
+                        output.accept(ModItems.ESSENCE);
+                        output.accept(ModItems.ARIEN_SWORD);
+                        output.accept(ModBlocks.ARIEN_BLOCK);
+
+                    }).build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TAB.register(eventBus);
+    }
+}
